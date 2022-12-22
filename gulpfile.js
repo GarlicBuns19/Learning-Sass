805663aeb81f930821fc00cc239f05d182fd3dc1
GarlicBuns19 <planterx26@gmail.com>
2022-12-22 08:05:13 -0800

    purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
-function buildStyles(){
-    return src('library_sass/**/*.scss')
-           .pipe(sass())
-           .pipe(dest('css')) 
+function buildStyles() {
+  return src("library_sass/**/*.scss")
+        .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
+        .pipe(dest("css"));
 }
 
-function watchTask(){
-    watch(['library_sass/**/*.scss'],buildStyles)
+function watchTask() {
+  watch(["library_sass/**/*.scss",'*.html'], buildStyles);
 }
 
-exports.default = series(buildStyles,watchTask)
+exports.default = series(buildStyles, watchTask);
